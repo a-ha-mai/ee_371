447b2ebc271ff7e7288fdb59501462b729a1689d
a-ha-mai <a.hoang.anh.mai@gmail.com>
2023-10-02 23:49:18 -0700

    parking_lot_occupancy_copy.sv is working as intended on labsland with onboard hardware, need to get external hardware working on parking_lot_occupancy.sv

--- lab1/parking_lot_occupancy_copy.sv
+++ lab1/parking_lot_occupancy_copy.sv
@@ -1,13 +1,15 @@
-module parking_lot_occupancy (V_GPIO, CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
-	inout logic [35:0] V_GPIO;
+module parking_lot_occupancy_copy (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW);
 	input logic CLOCK_50;
+	output logic [9:0] LEDR;
+	input logic [3:0] KEY;
+	input logic [9:0] SW;
 	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
 	logic [4:0] car_count;
-		
-	assign V_GPIO[31] = V_GPIO[28]; // outer led indicator
-	assign V_GPIO[33] = V_GPIO[29]; // inner led indicator
 	
-	car_counter cc(.reset(V_GPIO[30]), .clk(CLOCK_50), .outer(V_GPIO[28]), .inner(V_GPIO[29]), .car_count);
+	assign LEDR[0] = SW[0]; // outer led indicator
+	assign LEDR[1] = SW[1]; // inner led indicator
+	
+	car_counter cc(.reset(SW[9]), .clk(CLOCK_50), .outer(SW[0]), .inner(SW[1]), .car_count);
 	
 	always_comb begin
 		case (car_count)
@@ -167,76 +169,5 @@ module parking_lot_occupancy (V_GPIO, CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HE
 				HEX5 = 7'b1111111;
 			end
 		endcase
-	end
-endmodule
-
-module parking_lot_occupancy_tb ();
-	wire [35:0] V_GPIO;
-	logic [35:0] V_GPIO_in; 
-	logic [35:0] V_GPIO_dir;
-	logic CLOCK_50, clk;
-	logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
-	logic [4:0] car_count;
-	
-	genvar i; 
-	generate 
-		for (i = 0; i < 36; i++) begin : gpio 
-			assign V_GPIO[i] = V_GPIO_dir[i] ? V_GPIO_in[i] : 1'bZ; 
-		end 
-	endgenerate
-	
-	parking_lot_occupancy dut (.V_GPIO, .CLOCK_50, .HEX0, .HEX1, .HEX2, .HEX3, .HEX4, .HEX5);
-	
-	parameter CLOCK_PERIOD = 100;
-	initial begin
-		clk <= 0;
-		forever #(CLOCK_PERIOD/2) clk <= ~clk;
-	end
-	
-	initial begin
-		V_GPIO_dir[28] = 1'b1;
-		V_GPIO_dir[29] = 1'b1;
-		V_GPIO_dir[30] = 1'b1;
-		
-		V_GPIO_dir[31] = 1'b0;
-		V_GPIO_dir[33] = 1'b0;
-	end
-	
-	initial begin
-															 repeat (1) @(posedge clk);
-		V_GPIO_in[30] <= 1;
-		V_GPIO_in[28] <= 0; V_GPIO_in[29] <= 0; repeat (1) @(posedge clk);
-		V_GPIO_in[30] <= 0;							 repeat (1) @(posedge clk);
-		
-		V_GPIO_in[28] <= 1; V_GPIO_in[29] <= 0; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 1; V_GPIO_in[29] <= 1; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 0; V_GPIO_in[29] <= 1; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 0; V_GPIO_in[29] <= 0; repeat (3) @(posedge clk); // 1
-		
-		V_GPIO_in[28] <= 1; V_GPIO_in[29] <= 0; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 1; V_GPIO_in[29] <= 1; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 0; V_GPIO_in[29] <= 1; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 0; V_GPIO_in[29] <= 0; repeat (3) @(posedge clk); // 2
-		
-		V_GPIO_in[28] <= 0; V_GPIO_in[29] <= 1; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 1; V_GPIO_in[29] <= 1; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 1; V_GPIO_in[29] <= 0; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 0; V_GPIO_in[29] <= 0; repeat (3) @(posedge clk); // 1
-		
-		V_GPIO_in[28] <= 0; V_GPIO_in[29] <= 1; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 1; V_GPIO_in[29] <= 1; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 1; V_GPIO_in[29] <= 0; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 0; V_GPIO_in[29] <= 0; repeat (3) @(posedge clk); // 0
-		
-		V_GPIO_in[28] <= 0; V_GPIO_in[29] <= 1; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 1; V_GPIO_in[29] <= 1; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 1; V_GPIO_in[29] <= 0; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 0; V_GPIO_in[29] <= 0; repeat (3) @(posedge clk); // 0
-		
-		V_GPIO_in[28] <= 1; V_GPIO_in[29] <= 0; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 1; V_GPIO_in[29] <= 1; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 0; V_GPIO_in[29] <= 1; repeat (1) @(posedge clk);
-		V_GPIO_in[28] <= 0; V_GPIO_in[29] <= 0; repeat (3) @(posedge clk); // 1
-		$stop;
 	end
 endmodule
