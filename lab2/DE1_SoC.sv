170d94bcdbbc4b78ae6d2546b2713a5bcbf72b8e
a-ha-mai <a.hoang.anh.mai@gmail.com>
2023-10-08 22:02:30 -0700

    task 2 tb made, but not simming properly

---
+++ lab2/DE1_SoC.sv
@@ -0,0 +1,67 @@
+`timescale 1 ps / 1 ps
+
+module DE1_SoC (KEY, SW, HEX0, HEX1, HEX4, HEX5);
+	input logic [3:0] KEY;
+	input logic [9:0] SW;
+	output logic [6:0] HEX0, HEX1, HEX4, HEX5;
+	
+	logic [2:0] DataIn, DataOut;
+	logic [3:0] h0, h1, h4, h5;
+	logic [4:0] Address;
+	
+	task2 t2 (.address(Address), .clk(KEY[0]), .reset(SW[9]), .data(DataIn), .wren(SW[0]), .q(DataOut));
+	
+	seg7 hex0 (.hex(h0), .leds(HEX0));
+	seg7 hex1 (.hex(h1), .leds(HEX1));
+	seg7 hex4 (.hex(h4), .leds(HEX4));
+	seg7 hex5 (.hex(h5), .leds(HEX5));
+	
+	always_comb begin
+		DataIn = {SW[3], SW[2], SW[1]};
+		Address = {SW[8], SW[7], SW[6], SW[5], SW[4]};
+		h5 = Address[4];
+		case (Address[3:0])
+			4'b0000: h4 = 1'h0;
+			4'b0001: h4 = 1'h1;
+			4'b0010: h4 = 1'h2;
+			4'b0011: h4 = 1'h3;
+			4'b0100: h4 = 1'h4;
+			4'b0101: h4 = 1'h5;
+			4'b0110: h4 = 1'h6;
+			4'b0111: h4 = 1'h7;
+			4'b1000: h4 = 1'h8;
+			4'b1001: h4 = 1'h9;
+			4'b1010: h4 = 1'hA;
+			4'b1011: h4 = 1'hB;
+			4'b1100: h4 = 1'hC;
+			4'b1101: h4 = 1'hD;
+			4'b1110: h4 = 1'hE;
+			4'b1111: h4 = 1'hF;
+			default: h4 = 1'h0;
+		endcase
+		
+		case (DataIn)
+			4'b0000: h1 = 1'h0;
+			4'b0001: h1 = 1'h1;
+			4'b0010: h1 = 1'h2;
+			4'b0011: h1 = 1'h3;
+			4'b0100: h1 = 1'h1;
+			4'b0101: h1 = 1'h5;
+			4'b0110: h1 = 1'h6;
+			4'b0111: h1 = 1'h7;
+			default: h1 = 1'h0;
+		endcase
+		
+		case (DataOut)
+			4'b0000: h1 = 1'h0;
+			4'b0001: h1 = 1'h1;
+			4'b0010: h1 = 1'h2;
+			4'b0011: h1 = 1'h3;
+			4'b0100: h1 = 1'h1;
+			4'b0101: h1 = 1'h5;
+			4'b0110: h1 = 1'h6;
+			4'b0111: h1 = 1'h7;
+			default: h1 = 1'h0;
+		endcase
+	end
+endmodule
---
+++ lab2/DE1_SoC_tb.sv
@@ -0,0 +1,174 @@
+`timescale 1 ps / 1 ps
+
+module DE1_SoC_tb ();
+	logic [3:0] KEY;
+	logic [9:0] SW;
+	logic [6:0] HEX0, HEX1, HEX4, HEX5;
+	
+	logic clk;
+	logic [2:0] DataIn, DataOut;
+	logic [3:0] h0, h1, h4, h5;
+	logic [4:0] Address;
+	
+	DE1_SoC dut (.KEY, .SW, .HEX0, .HEX1, .HEX4, .HEX5);
+	task2 dut2 (.address(Address), .clk(clk), .reset(SW[9]), .data(DataIn), .wren(SW[0]), .q(DataOut));
+	
+	parameter CLOCK_PERIOD = 100;
+	
+	// initialize signals
+	initial begin
+		forever #(CLOCK_PERIOD/2) clk <= ~clk;
+	end
+	
+	initial begin
+		@(posedge clk);
+		
+		SW[9] <= 1;
+		SW[8] <= 0;
+		SW[7] <= 0;
+		SW[6] <= 0;
+		SW[5] <= 0;
+		SW[4] <= 0;
+		SW[3] <= 0;
+		SW[2] <= 0;
+		SW[1] <= 0;
+		SW[0] <= 0;
+		@(posedge clk);
+		SW[9] <= 0;
+		@(posedge clk);
+		
+		// write operation test
+		SW[8] <= 0; // set Address for writing
+		SW[7] <= 0;
+		SW[6] <= 0;
+		SW[5] <= 0;
+		SW[4] <= 0;
+		SW[3] <= 0; // set DataIn to be written
+		SW[2] <= 1;
+		SW[1] <= 1;
+		SW[0] <= 1; // enable write operation
+		repeat (2) @(posedge clk);
+		
+		// read operation test
+		SW[8] <= 0; // set Address for writing
+		SW[7] <= 0;
+		SW[6] <= 0;
+		SW[5] <= 0;
+		SW[4] <= 0;
+		SW[3] <= 0; // set DataIn to be written
+		SW[2] <= 1;
+		SW[1] <= 1;
+		SW[0] <= 0; // disable write operation
+		repeat (2) @(posedge clk);
+		
+		// changing Address test
+		SW[8] <= 0; // set Address for writing
+		SW[7] <= 0;
+		SW[6] <= 0;
+		SW[5] <= 1;
+		SW[4] <= 0;
+		SW[3] <= 0; // set DataIn to be written
+		SW[2] <= 0;
+		SW[1] <= 1;
+		SW[0] <= 0;
+		repeat (2) @(posedge clk);
+		
+		// write test
+		SW[8] <= 0; // set Address for writing
+		SW[7] <= 0;
+		SW[6] <= 0;
+		SW[5] <= 1;
+		SW[4] <= 0;
+		SW[3] <= 0; // set DataIn to be written
+		SW[2] <= 0;
+		SW[1] <= 1;
+		SW[0] <= 1;
+		repeat (2) @(posedge clk);
+		
+		// read test
+		SW[8] <= 0; // set Address for writing
+		SW[7] <= 0;
+		SW[6] <= 0;
+		SW[5] <= 1;
+		SW[4] <= 0;
+		SW[3] <= 0; // set DataIn to be written
+		SW[2] <= 0;
+		SW[1] <= 1;
+		SW[0] <= 0;
+		Address <= 2; 
+		DataIn <= 1;
+		repeat (2) @(posedge clk);
+		
+		// changing Address test
+		SW[8] <= 0; // set Address for writing
+		SW[7] <= 0;
+		SW[6] <= 1;
+		SW[5] <= 0;
+		SW[4] <= 0;
+		SW[3] <= 0; // set DataIn to be written
+		SW[2] <= 1;
+		SW[1] <= 0;
+		SW[0] <= 0;
+		repeat (2) @(posedge clk);
+		
+		// write test
+		SW[8] <= 0; // set Address for writing
+		SW[7] <= 0;
+		SW[6] <= 1;
+		SW[5] <= 0;
+		SW[4] <= 0;
+		SW[3] <= 0; // set DataIn to be written
+		SW[2] <= 1;
+		SW[1] <= 0;
+		SW[0] <= 1;
+		repeat (2) @(posedge clk);
+		
+		// changing DataIn when write enabled test
+		SW[8] <= 0; // set Address for writing
+		SW[7] <= 0;
+		SW[6] <= 1;
+		SW[5] <= 0;
+		SW[4] <= 0;
+		SW[3] <= 1; // set DataIn to be written
+		SW[2] <= 0;
+		SW[1] <= 1;
+		SW[0] <= 1;
+		repeat (2) @(posedge clk);
+		
+		SW[8] <= 0; // set Address for writing
+		SW[7] <= 0;
+		SW[6] <= 1;
+		SW[5] <= 0;
+		SW[4] <= 0;
+		SW[3] <= 1; // set DataIn to be written
+		SW[2] <= 0;
+		SW[1] <= 1;
+		SW[0] <= 0;
+		repeat (2) @(posedge clk);
+		
+		// changing DataIn when write disabled test
+		SW[8] <= 0; // set Address for writing
+		SW[7] <= 0;
+		SW[6] <= 1;
+		SW[5] <= 0;
+		SW[4] <= 0;
+		SW[3] <= 0; // set DataIn to be written
+		SW[2] <= 1;
+		SW[1] <= 0;
+		SW[0] <= 0;
+		repeat (2) @(posedge clk);
+		
+		// check first Address
+		SW[8] <= 0; // set Address for writing
+		SW[7] <= 0;
+		SW[6] <= 0;
+		SW[5] <= 0;
+		SW[4] <= 0;
+		SW[3] <= 0; // set DataIn to be written
+		SW[2] <= 0;
+		SW[1] <= 0;
+		SW[0] <= 0;
+		repeat (2) @(posedge clk);
+		$stop;
+	end
+endmodule
