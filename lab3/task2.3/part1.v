4dfb732dace80705ac744224c745a8b420b08dd8
a-ha-mai <a.hoang.anh.mai@gmail.com>
2023-10-18 11:34:04 -0700

    up to task 2.3

---
+++ lab3/task2.3/part1.v
@@ -0,0 +1,104 @@
+module part1 (CLOCK_50, CLOCK2_50, KEY, FPGA_I2C_SCLK, FPGA_I2C_SDAT, AUD_XCK, 
+				  AUD_DACLRCK, AUD_ADCLRCK, AUD_BCLK, AUD_ADCDAT, AUD_DACDAT);
+
+	input CLOCK_50, CLOCK2_50;
+	input [0:0] KEY;
+	// I2C Audio/Video config interface
+	output FPGA_I2C_SCLK;
+	inout FPGA_I2C_SDAT;
+	// Audio CODEC
+	output AUD_XCK;
+	input AUD_DACLRCK, AUD_ADCLRCK, AUD_BCLK;
+	input AUD_ADCDAT;
+	output AUD_DACDAT;
+	
+	// Local wires.
+	wire read_ready, write_ready, read, write;
+	wire [23:0] readdata_left, readdata_right, readdata;
+	wire [23:0] writedata_left, writedata_right;
+	wire reset = ~KEY[0];
+
+	/////////////////////////////////
+	// Your code goes here 
+	/////////////////////////////////
+	
+	// Create an address counter.
+	reg [14:0] address = 15'h0000;  // 18-bit address counter, initialize with 0
+
+	// Increment address counter when writing to FPGA
+	always @(posedge CLOCK_50) begin
+		if (reset) begin
+			address <= 15'h0000;
+		end else if (write) begin
+			address <= address + 1;
+			if (address == 15'hBB80) begin
+				address <= 15'h0000;
+			end
+		end
+	end
+	
+	C4 tone (.address(address), .clock(CLOCK_50), .q(readdata));
+	
+	assign writedata_left = readdata;
+	assign writedata_right = readdata;
+	assign read = read_ready & write_ready;
+	assign write = read_ready & write_ready;
+	
+	
+/////////////////////////////////////////////////////////////////////////////////
+// Audio CODEC interface. 
+//
+// The interface consists of the following wires:
+// read_ready, write_ready - CODEC ready for read/write operation 
+// readdata_left, readdata_right - left and right channel data from the CODEC
+// read - send data from the CODEC (both channels)
+// writedata_left, writedata_right - left and right channel data to the CODEC
+// write - send data to the CODEC (both channels)
+// AUD_* - should connect to top-level entity I/O of the same name.
+//         These signals go directly to the Audio CODEC
+// I2C_* - should connect to top-level entity I/O of the same name.
+//         These signals go directly to the Audio/Video Config module
+/////////////////////////////////////////////////////////////////////////////////
+	clock_generator my_clock_gen(
+		// inputs
+		CLOCK2_50,
+		reset,
+
+		// outputs
+		AUD_XCK
+	);
+
+	audio_and_video_config cfg(
+		// Inputs
+		CLOCK_50,
+		reset,
+
+		// Bidirectionals
+		FPGA_I2C_SDAT,
+		FPGA_I2C_SCLK
+	);
+
+	audio_codec codec(
+		// Inputs
+		CLOCK_50,
+		reset,
+
+		read,	write,
+		writedata_left, writedata_right,
+
+		AUD_ADCDAT,
+
+		// Bidirectionals
+		AUD_BCLK,
+		AUD_ADCLRCK,
+		AUD_DACLRCK,
+
+		// Outputs
+		read_ready, write_ready,
+		readdata_left, readdata_right,
+		AUD_DACDAT
+	);
+
+endmodule
+
+
